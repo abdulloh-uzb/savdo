74e489822bc1048c897861bef4c3d93b3ddcedeb
abdulloh-uzb <uabdulloh11@gmail.com>
2024-08-30 02:12:53 +0500

    Data for the filter is no longer coming from main page

--- carpets.php
+++ carpets.php
@@ -2,29 +2,32 @@
 require "database.php";
 require "functions.php";
 
-if (!empty($_GET)) {
-    $width = $_GET['width'];
-    $height = $_GET['height'];
 
-    $carpets = $db->read('carpets', ['width' => $width, 'height' => $height]);
-}
-
+$carpets = $db->read('carpets');
 ?>
 
-<ul>
-    <? if (isset($carpets)): ?>
-        <?php foreach ($carpets as $carpet): ?>
-            <li>
-                <span><?= $carpet['title'] ?></span>
+
+
+<table>
+    <tr>
+        <th>Nomi</th>
+        <th>Narxi</th>
+        <th>Soni</th>
+        <th>Amal</th>
+    </tr>
+    <?php foreach ($carpets as $carpet): ?>
+        <tr>
+            <td><?= $carpet['title'] ?></td>
+            <td><?= $carpet['price'] ?></td>
+            <td><?= $carpet['qty'] ?></td>
+            <td>
                 <form action="buy.php" method="post">
                     <input type="text" name="id" value="<?= $carpet['id'] ?>" hidden>
                     <button type="submit">Sotish</button>
                 </form>
-            </li>
-        <?php endforeach; ?>
-    <? else: ?>
-        <p>
-            Bunaqa gilam yoq
-        </p>
-    <? endif; ?>
-</ul>
+            </td>
+        </tr>
+
+
+    <?php endforeach; ?>
+</table>
