196d5a807f671bb9bfb0ccf157b2bbc8afc3b02e
abdulloh-uzb <uabdulloh11@gmail.com>
2024-08-30 02:36:45 +0500

    added filter to check data coming from user

--- main.php
+++ main.php
@@ -1,11 +1,14 @@
 <?php
 require "database.php";
+require "functions.php";
 
 if (!empty($_GET)) {
-    $width = $_GET['width'];
-    $height = $_GET['height'];
+    $width = filter_input(INPUT_GET,'width', FILTER_SANITIZE_NUMBER_INT);
+    $height = filter_input(INPUT_GET,'height', FILTER_SANITIZE_NUMBER_INT);
+    
+    $data = ['width' => $width, 'height' => $height];
 
-    $carpets = $db->read('carpets', ['width' => $width, 'height' => $height]);
+    $carpets = $db->read('carpets', $data);
 }
 
 
@@ -20,35 +23,37 @@ if (!empty($_GET)) {
     <button type="submit">Qidirish</button>
 </form>
 
-<? if (isset($carpets)): ?>
-    <table>
-        <tr>
-            <th>Nomi</th>
-            <th>Narxi</th>
-            <th>Soni</th>
-            <th>Amal</th>
-        </tr>
-        <?php foreach ($carpets as $carpet): ?>
+<? if (!empty($_GET)): ?>
+    <? if (!empty($carpets) && is_array($carpets)): ?>
+        <table>
             <tr>
-                <td><?= $carpet['title'] ?></td>
-                <td><?= $carpet['price'] ?></td>
-                <td><?= $carpet['qty'] ?></td>
-                <td>
-                    <form action="buy.php" method="post">
-                        <input type="text" name="id" value="<?= $carpet['id'] ?>" hidden>
-                        <button type="submit">Sotish</button>
-                    </form>
-                </td>
+                <th>Nomi</th>
+                <th>Narxi</th>
+                <th>Soni</th>
+                <th>Amal</th>
             </tr>
+            <?php foreach ($carpets as $carpet): ?>
+                <tr>
+                    <td><?= $carpet['title'] ?></td>
+                    <td><?= $carpet['price'] ?></td>
+                    <td><?= $carpet['qty'] ?></td>
+                    <td>
+                        <form action="buy.php" method="post">
+                            <input type="text" name="id" value="<?= $carpet['id'] ?>" hidden>
+                            <button type="submit">Sotish</button>
+                        </form>
+                    </td>
+                </tr>
 
 
-        <?php endforeach; ?>
-    </table>
+            <?php endforeach; ?>
+        </table>
 
-<? else: ?>
-    <p>
-        Bunaqa gilam yoq
-    </p>
+    <? else: ?>
+        <p>
+            Bunaqa gilam yoq
+        </p>
+    <? endif; ?>
 <? endif; ?>
 
 <a href="add.php">Gilam qo'shish</a>
