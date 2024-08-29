87e885b2f13a6847a309bfa4a84de91994cf5179
abdulloh-uzb <uabdulloh11@gmail.com>
2024-08-30 02:13:50 +0500

    Added search and table to show search carpets

--- main.php
+++ main.php
@@ -1,9 +1,18 @@
 <?php
 require "database.php";
 
+if (!empty($_GET)) {
+    $width = $_GET['width'];
+    $height = $_GET['height'];
+
+    $carpets = $db->read('carpets', ['width' => $width, 'height' => $height]);
+}
+
+
 ?>
 <h1>Gilam qidirish</h1>
-<form action="carpets.php" method="GET">
+
+<form action="main.php" method="GET">
     <label for="">Eni</label>
     <input type="text" name="width">
     <label for="">Bo'yi</label>
@@ -11,5 +20,35 @@ require "database.php";
     <button type="submit">Qidirish</button>
 </form>
 
+<? if (isset($carpets)): ?>
+    <table>
+        <tr>
+            <th>Nomi</th>
+            <th>Narxi</th>
+            <th>Soni</th>
+            <th>Amal</th>
+        </tr>
+        <?php foreach ($carpets as $carpet): ?>
+            <tr>
+                <td><?= $carpet['title'] ?></td>
+                <td><?= $carpet['price'] ?></td>
+                <td><?= $carpet['qty'] ?></td>
+                <td>
+                    <form action="buy.php" method="post">
+                        <input type="text" name="id" value="<?= $carpet['id'] ?>" hidden>
+                        <button type="submit">Sotish</button>
+                    </form>
+                </td>
+            </tr>
+
+
+        <?php endforeach; ?>
+    </table>
+
+<? else: ?>
+    <p>
+        Bunaqa gilam yoq
+    </p>
+<? endif; ?>
 
 <a href="add.php">Gilam qo'shish</a>
