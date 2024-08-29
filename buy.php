7d4a128922db0cbb5640fdcfad2ab82350058c10
abdulloh-uzb <uabdulloh11@gmail.com>
2024-08-30 02:10:14 +0500

    used header to come back previous page

--- buy.php
+++ buy.php
@@ -7,5 +7,6 @@ if (isset($_POST)) {
     $carpet = $db->read('carpets', ['id' => $id]);
     $qty = $carpet[0]['qty'] - 1;
     $db->update("carpets", ['qty' => $qty], ['id' => $id]);
+    header('Location: ' . $_SERVER['HTTP_REFERER']);
 }
 
